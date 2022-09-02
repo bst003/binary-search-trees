f6b30f9671f6c2db27e23ea47aa39eb6435e815b
bst003 <bstanner33@yahoo.com>
2022-09-01 22:53:22 -0400

    Set up methof to sort and reduce array

--- assets/main.js
+++ assets/main.js
@@ -15,18 +15,60 @@ const Node = (() => {
 })();
 
 const Tree = () => {
-    let tree = {
+    let _tree = {
         root: {},
     };
 
-    const logTree = () => {
-        console.log(tree);
+    const _removeDuplicates = (array) => {
+        const newArray = array.reduce((previousValue, currentValue) => {
+            if (previousValue.indexOf(currentValue) === -1) {
+                previousValue.push(currentValue);
+            }
+            return previousValue;
+        }, []);
+
+        return newArray;
     };
 
-    const buildTree = (array) => {};
+    const logTree = () => {
+        console.log(_tree);
+    };
+
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+        if (node.right !== null) {
+            prettyPrint(
+                node.right,
+                `${prefix}${isLeft ? "│   " : "    "}`,
+                false
+            );
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            prettyPrint(
+                node.left,
+                `${prefix}${isLeft ? "    " : "│   "}`,
+                true
+            );
+        }
+    };
+
+    const buildTree = (array) => {
+        array.sort((a, b) => b - a);
+        console.log(array);
+
+        const reducedArray = _removeDuplicates(array);
+        console.log(reducedArray);
+    };
 
     return {
         logTree,
         buildTree,
+        prettyPrint,
     };
 };
+
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const newTree = Tree();
+
+newTree.buildTree(testArray);
